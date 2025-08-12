0fd8dd8ff292fd33b31c17ff5288e6086c20eb26
DMCruspe <Di16ma88@gmail.com>
2025-08-12 19:03:27 +0300

    0.11.2

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

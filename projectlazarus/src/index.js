c3c0115f1d33d1fbf339c100f465530f717f8310
DMCruspe <Di16ma88@gmail.com>
2025-08-19 18:51:57 +0300

    0611

---
+++ projectlazarus/src/index.js
@@ -0,0 +1,12 @@
+ srcindex.js
+import React from 'react';
+import ReactDOM from 'react-domclient';
+import '.index.css';  Optional import global CSS styles
+import App from '.App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  React.StrictMode
+    App 
+  React.StrictMode
+);
